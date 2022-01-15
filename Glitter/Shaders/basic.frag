865059c716149a3f13c31a0e81929984ff883e4c
gabrieljreed <gabrieljreed@gmail.com>
2022-01-15 13:08:34 -0700

    Make shaders come from separate files, clean up main.cpp

--- Glitter/Shaders/basic.frag
+++ Glitter/Shaders/basic.frag
@@ -5,3 +5,10 @@ void main()
 {
     ColorOut = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
+
+/*"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\n\0" */
--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -13,18 +13,6 @@
 #include <cstdlib>
 #include <iostream>
 
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-"}\n\0";
 
 int main(int argc, char * argv[]) {
     
@@ -58,45 +46,8 @@ int main(int argc, char * argv[]) {
     glEnable(GL_DEPTH_TEST);
     
     
-    // MARK: Vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    
-    // MARK: Fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    
-    // MARK: Link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    // MARK: Import my shader code
+    GLuint myShader = LoadProgram("/Users/gabrieljreed/Glitter/Glitter/Shaders/basic.vert", "/Users/gabrieljreed/Glitter/Glitter/Shaders/basic.frag");
     
     
     
@@ -149,7 +100,7 @@ int main(int argc, char * argv[]) {
         // **********************************
         // Add rendering code here
         
-        glUseProgram(shaderProgram);
+        glUseProgram(myShader);
         glBindVertexArray(VAO); // Bind VAO
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
         glBindVertexArray(0); // Unvind VAO - you don't have to do these steps here because there's only one VAO
